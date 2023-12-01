cad205920354cd1816680afe999f9b897c1c319e
Michael Quintero <michaelq.quintero@gmail.com>
2023-12-01 18:00:33 -0500

    Add dns support to vpc

--- main.tf
+++ main.tf
@@ -5,6 +5,8 @@ provider "aws" {
 # 1. create vpc
 resource "aws_vpc" "wemerch" {
     cidr_block = "10.0.0.0/16"
+    enable_dns_support = true
+    enable_dns_hostnames = true
     tags = {
         Name = "production"
     }
