cf20fec17faff585db9425c3c7facdbf4c07728e
Michael Quintero <michaelq.quintero@gmail.com>
2023-12-05 22:17:40 -0500

    Enable CORS on api gateway

--- main.tf
+++ main.tf
@@ -148,3 +148,10 @@ resource "aws_nat_gateway" "wemerch_nat_gateway" {
     Name = "wemerch-nat-gateway"
   }
 }
+
+module "api-gateway-enable-cors" {
+source  = "squidfunk/api-gateway-enable-cors/aws"
+version = "0.3.3"
+api_id          = aws_api_gateway_rest_api.wemerch_api_gateway_rest_api.id
+api_resource_id = aws_api_gateway_method.proxy.resource_id
+}
